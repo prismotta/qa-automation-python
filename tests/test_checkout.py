eeb8c0568ffd90904d99c8b768a9a6c15c693594
Priscila <42475127+prismotta@users.noreply.github.com>
2026-01-27 13:34:20 -0300

    Link checkout test to test case CT-CHECKOUT-02

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -1,2 +1,9 @@
 def test_checkout_com_dados_validos():  # CT-CHECKOUT-02
+    """
+    Caso de teste automatizado referente ao:
+    CT-CHECKOUT-02 — Finalizar checkout com dados válidos
+
+    Casos de teste documentados em:
+    https://github.com/prismotta/qa-test-cases/tree/main/checkout
+    """
     pass
